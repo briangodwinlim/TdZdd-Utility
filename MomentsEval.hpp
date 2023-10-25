2fe91506b55b6a850af3b251c285034aedd496e5
Brian Godwin Lim <brian@brianlim.xyz>
2023-10-25 12:55:55 +0900

    Added normalized moments

--- MomentsEval.hpp
+++ MomentsEval.hpp
@@ -14,6 +14,7 @@ template<typename V = int, typename T = V>
 class MomentsEval: public DdEval<MomentsEval<V,T>,std::vector<V>> {
 private:
     int const n;
+    V const offset, scale;
     std::vector<T> const val;
 
     V ncr(int n, int k) {
@@ -22,13 +23,17 @@ private:
     }
 
 public:
-    MomentsEval(std::vector<T> val, int n) 
-         : val(val), n(n) {
+    MomentsEval(std::vector<T> val, int n, V offset = 0, V scale = 1) 
+         : val(val), n(n), offset(offset), scale(scale) {
     }
 
     void evalTerminal(std::vector<V> &v, int id) {
         v = std::vector<V>(n + 1, 0);
-        v[0] = id ? 1 : 0;
+        if (id) {
+            for (int i = 0; i <= n; ++i) {
+                v[i] = std::pow(offset * scale, i);
+            }
+        }
     }
 
     void evalNode(std::vector<V> &v, int level, DdValues<std::vector<V>,2> const& values) {
@@ -37,7 +42,7 @@ public:
         for (int n_ = 0; n_ <= n; ++n_) {
             v[n_] = values.get(0)[n_];
             for (int p = 0; p <= n_; ++p) {
-                v[n_] += ncr(n_, p) * values.get(1)[n_ - p] * pow(val[level], p);
+                v[n_] += ncr(n_, p) * values.get(1)[n_ - p] * pow(val[level] * scale, p);
             }
         }
     }
